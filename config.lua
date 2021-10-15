c4179e54714269b906596ca5bb34b3d706b1ac7f
Daniel Reed <x@sys32.dev>
2021-10-15 17:30:45 -0400

    config

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 local CONFIG = {}
 
-CONFIG.token = "ODg0MjQ3MjM4MjkxODkwMjM2.YTVtag.qaPr5C9mKx6YefiWyG4S1yr00BQ"
-CONFIG.apodkey = "QUPEeDnQl4cJ1rQ5gIqgJTMb5z5OeG3I7y2Kk3lP"
+CONFIG.token = "TOKEN HERE"
+CONFIG.apodkey = "APOD KEY"
 CONFIG.commandEvent = true
 
 return CONFIG
