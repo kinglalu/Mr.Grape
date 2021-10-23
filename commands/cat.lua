277546916b95a1a0407ff1761042562eda7497a9
kinglalu <56965132+kinglalu@users.noreply.github.com>
2021-10-22 21:44:06 -0700

    fix +cat

--- commands/cat.lua
+++ commands/cat.lua
@@ -1,12 +1,11 @@
 command.Register("cat", "Get a random cat image!","fun",function(msg, args)
-    local res, data = CORO.request("GET", "https://api.thecatapi.com/v1/images/search")
-    local key,value = unpack(res)
-    local datvar = string.split((tostring(data)),'"')
-    -- url is datvar[10]
+    --if command.Cooldown(msg, "cat", 2, "Look, ik cats are cute, but  wait **%s** seconds dude") then return end
+    local res, data = CORO.request("GET", "https://api.thecatapi.com/v1/images/search?limit=1&size=full&mime_types=jpg,png")
+    local image = JSON.parse(data)
     msg:reply({
         embed = {
           title = ":cat: Meow!",
-          image = {url =  datvar[10]},
+          image = {url =  image[1]["url"]},
           color = DISCORDIA.Color.fromRGB(170, 26, 232).value,
           timestamp = DISCORDIA.Date():toISO('T', 'Z')
           }
