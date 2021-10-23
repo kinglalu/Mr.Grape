d53e09f816de1e0e5d9f3e09b76e5f7f56acdcc6
kinglalu <56965132+kinglalu@users.noreply.github.com>
2021-10-22 21:57:49 -0700

    add +apod!

--- commands/apod.lua
+++ commands/apod.lua
@@ -1,3 +1,13 @@
 command.Register("apod", "Sends Nasa's pic of the day","fun",function(msg, args)
-   
+    local res, data = CORO.request("GET", "https://api.nasa.gov/planetary/apod?api_key="..CONFIG.apodkey)
+    local image = JSON.parse(data)
+    msg:reply({
+        embed = {
+          title = "NASA's Picture of the Day!",
+          description = image["explanation"],
+          image = {url =  image["hdurl"]},
+          color = DISCORDIA.Color.fromRGB(170, 26, 232).value,
+          timestamp = DISCORDIA.Date():toISO('T', 'Z')
+          }
+      })
 end)
