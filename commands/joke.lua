589f3d045e91ddf10e3c4fa3e2fa817e5c94e77b
kinglalu <56965132+kinglalu@users.noreply.github.com>
2021-10-28 06:38:06 -0700

    Add +joke!

--- commands/joke.lua
+++ commands/joke.lua
@@ -2,6 +2,7 @@ command.Register("joke", "Get a random joke!","fun",function(msg, args)
     --if command.Cooldown(msg, "joke", 2, "I know you love my jokes, but  wait **%s** seconds dude") then return end
     local res, data = CORO.request("GET", "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart")
     local joke = JSON.parse(data)
+    
     msg:reply({
         embed = {
           title = "Joke:",
