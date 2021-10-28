4b5a57ee74b1d03ec6c55afd8a09ec0b23390ff9
kinglalu <56965132+kinglalu@users.noreply.github.com>
2021-10-28 06:46:31 -0700

    small change to +fact

--- commands/fact.lua
+++ commands/fact.lua
@@ -1,5 +1,5 @@
 command.Register("fact", "Get a random fact!","fun",function(msg, args)
-    --if command.Cooldown(msg, "joke", 2, "I know these facts are intresting, but  wait **%s** seconds dude") then return end
+    --if command.Cooldown(msg, "fact", 2, "I know these facts are intresting, but  wait **%s** seconds dude") then return end
     local res, data = CORO.request("GET", "https://useless-facts.sameerkumar.website/api")
     local fact = JSON.parse(data)
     
