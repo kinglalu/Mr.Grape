5f7d8e7ceb725e0694984a9cad7f531610f1279f
kinglalu <56965132+kinglalu@users.noreply.github.com>
2021-10-28 06:29:47 -0700

    Add +joke!

---
+++ commands/joke.lua
@@ -0,0 +1,16 @@
+command.Register("joke", "Get a random joke!","fun",function(msg, args)
+    --if command.Cooldown(msg, "joke", 2, "I know you love my jokes, but  wait **%s** seconds dude") then return end
+    local res, data = CORO.request("GET", "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart")
+    local joke = JSON.parse(data)
+    msg:reply({
+        embed = {
+          title = "Joke:",
+          description = joke["setup"],
+          fields = {
+            {name = "ㅤ", value = joke["delivery"] }
+          },
+          color = DISCORDIA.Color.fromRGB(170, 26, 232).value,
+          timestamp = DISCORDIA.Date():toISO('T', 'Z')
+        }
+      })
+end)
