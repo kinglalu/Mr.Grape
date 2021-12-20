32e12b10a8a89146f8f39bc25eb8bab4c9fe925a
kinglalu <56965132+kinglalu@users.noreply.github.com>
2021-12-20 12:58:20 -0800

    Add usdebt command, new function in functions.lua

---
+++ commands/usdebt.lua
@@ -0,0 +1,16 @@
+command.Register("usdebt", "Shows America's national debt to the penny.","fun",function(msg, args)
+    local res, data = CORO.request("GET", "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/debt_to_penny?sort=-record_date")
+    local debt = JSON.parse(data)
+    msg:reply({
+        embed = {
+          title = "America's debt to the penny!",
+          fields = {
+            {name = "**As of "..debt["data"][1].record_date.."**", value = "The US total public debt is \n **$"..FUNCTIONS.format_int(debt["data"][1].tot_pub_debt_out_amt).."**"},
+            {name = "ㅤ", value = "[Fiscal Data, Treasury.gov](https://fiscaldata.treasury.gov)"}
+        },
+
+          color = EMBEDCOLOR,
+          timestamp = DISCORDIA.Date():toISO('T', 'Z')
+          }
+      })
+end)
