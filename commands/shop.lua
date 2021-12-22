e2fec66facb34918e96900f8c423e59fba424902
kinglalu <56965132+kinglalu@users.noreply.github.com>
2021-12-22 10:36:04 -0800

    change to +shop

--- commands/shop.lua
+++ commands/shop.lua
@@ -1,6 +1,8 @@
 command.Register("shop", "Displays the shop for items you can buy with :star:", "economy", function(msg, args)
     local DB = require('../handler/items.lua')
     local items = DB.KnownItems
+    local id = DB.CreateRowUser(msg.author)
+    local personitems = DB.GetUserItems(id)
     local embed = {
         title = "Shop",
         description = "`Buy an item by using "..PREFIX.."buy <itemname>!`",
@@ -10,6 +12,11 @@ command.Register("shop", "Displays the shop for items you can buy with :star:", 
     }
     
     for i,v in pairs(items) do
+       for g,b in pairs(personitems) do
+            if i == g then
+                v.price = v.price*b.quantity
+           end
+        end 
 		table.insert(embed.fields, {name=v.Emoji..i,value=v.description..'\n'..v.price..':star:'})
     end
     assert(msg:reply{embed = embed})
