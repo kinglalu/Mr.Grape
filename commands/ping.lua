71ce6afe9f00b241978f51747c5e4d3f70c7c987
kinglalu <56965132+kinglalu@users.noreply.github.com>
2022-02-01 16:14:08 +0100

    Improved ping command

--- commands/ping.lua
+++ commands/ping.lua
@@ -1,3 +1,23 @@
 command.Register("ping","Your basic ping command!","utility", function(msg, args)
-	msg.channel:send("Pong!")
+	local time1 = msg.createdAt
+	local msg2 = msg.channel:send{
+        embed = {
+            title = "Pong!",
+            description = "Getting ping...",
+            color = EMBEDCOLOR,
+            timestamp = DISCORDIA.Date():toISO('T', 'Z')
+          }
+    }
+	local time2 = msg2.createdAt
+	local ping = string.sub(tostring(time2-time1), 4, 6)
+	print(ping)
+	ping = string.sub(ping, 1, 1)..'.'..string.sub(ping,2,6)
+	local pe = {
+		title =  "Pong!",
+		description = 'Ping: ``'..ping..'ms``',
+		color = EMBEDCOLOR,
+		timestamp = DISCORDIA.Date():toISO('T', 'Z')
+	}
+	msg2:setEmbed(pe)
+	
 end)
